fed8d407f473b252e8c4524d480a74912f23790d
Javidan <javidanhaj@gmail.com>
2026-02-23 16:58:52 +0400

    work-in-progress: orders module

---
+++ src/main/java/com/aztelekom/internship/domain/entities/Order.java
@@ -0,0 +1,42 @@
+package com.aztelekom.internship.domain.entities;
+
+import com.aztelekom.internship.domain.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customerId;
+
+    @Column(name = "status")
+    private OrderStatus status;
+
+    @Column(name = "total_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
